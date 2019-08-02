581a2628f001fc4198f730896bcba8447f0199f1
KoTubA <42877222+KoTubA@users.noreply.github.com>
2019-08-02 18:31:16 +0200

    v1.3

--- visual-effects.js
+++ visual-effects.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
         $('#login-wrapper').toggleClass("hidden-el");
         $('#toggle-form').toggleClass("move-el");
     });
+
+    $('.icon-input-second').on('click', function() {
+        $('.icon-input-second i').toggleClass("icon-eye icon-eye-off");
+        $('.pr-input').attr('type', function(index, attr){
+            return attr == 'text' ? 'password' : 'text';
+        });
+    });
 });
