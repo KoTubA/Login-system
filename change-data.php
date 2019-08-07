aa25567755c29b475140e60ddd5815cbabcf577d
KoTubA <42877222+KoTubA@users.noreply.github.com>
2019-08-08 00:21:18 +0200

    New user panel

---
+++ change-data.php
@@ -0,0 +1,158 @@
+<?php
+
+    session_start();
+
+    if((!isset($_POST['data_login']))||(!isset($_POST['data_pass']))) {
+        header('Location: panel.php');
+        exit();
+    }
+    require_once('connect.php');
+    
+    $conn = @new mysqli($host, $db_user, $db_password, $db_name);
+    if ($conn->connect_errno) {
+        $_SESSION['d_error'] = "Error: ".$conn->connect_errno;
+    }
+    else {
+
+        $login = mysqli_real_escape_string($conn,$_POST['data_login']);
+        $mail = mysqli_real_escape_string($conn,$_POST['data_mail']);
+        $name = mysqli_real_escape_string($conn,$_POST['data_name']);
+        $surname = mysqli_real_escape_string($conn,$_POST['data_surname']);
+        $number = mysqli_real_escape_string($conn,$_POST['data_number']);
+        $pass = mysqli_real_escape_string($conn,$_POST['data_pass']);
+        $s_name = mysqli_real_escape_string($conn,$_POST['radio-stacked']);
+        //Chcek s_login and validate data
+
+        $id_user = $_SESSION['id_copy'];
+        
+        $sql = "SELECT * FROM `users` WHERE id='$id_user'"; 
+        if($result = @$conn->query($sql)) {
+            $resultCheck = $result->num_rows;
+                if($resultCheck > 0) {
+                    $row = $result->fetch_assoc();
+                    
+                    if($id_user==$row['id']) {
+
+                        $data_login = $row['login'];
+                        $data_mail = $row['mail'];
+                        $flag = true;
+
+                        //Login
+                        if (strlen($login)<5) {
+                            $flag = false;
+                            $_SESSION['edata_login'] = "Minimalna długość: 5";
+                        }
+                        else if (strlen($login)>16) {
+                            $flag = false;
+                            $_SESSION['edata_login'] = "Maksymalna długość: 16";
+                        }
+                        else if(!preg_match("/^[a-zA-Z0-9]+$/",$login)) {
+                            $flag = false;
+                            $_SESSION['edata_login'] = "Podaj poprawny login";
+                        }
+                        else {
+                            $sql = "SELECT * FROM `users` WHERE login='$login'";
+                            if($result = @$conn->query($sql)) {
+                                $resultCheck = $result->num_rows;
+                                
+                                if($resultCheck > 0) {
+                                    if($login!=$data_login) {
+                                        $flag = false;
+                                        $_SESSION['edata_login'] = "Login już istnieje";
+                                    }
+                                }
+
+                                $result->close();
+                            }
+                            else {
+                                $_SESSION['data_error'] = 'Error: Błąd zapytania do bazy!';
+                            }
+                        }
+
+                        //E-mail
+                        if(!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+                            $flag = false;
+                            $_SESSION['edata_mail'] = "Podaj właściwy adres e-mail";
+                        }
+                        else {
+                            $sql = "SELECT * FROM `users` WHERE mail='$mail'";
+                            if($resultMail = @$conn->query($sql)) {
+                                $resultMailCheck = $resultMail->num_rows;
+                                
+                                if($resultMailCheck > 0) {
+                                    if($mail!=$data_mail) {
+                                        $flag = false;
+                                        $_SESSION['edata_mail'] = "Adres e-mail już istnieje";
+                                    }
+                                }
+
+                                $resultMail->close();
+                            }
+                            else {
+                                $_SESSION['data_error'] = 'Error: Błąd zapytania do bazy!';
+                            }
+                        }
+
+                        //Number
+                        if (strlen($number)!=9) {
+                            if($number!="") {
+                                $flag = false;
+                                $_SESSION['edata_number'] = "Wymagana ilość znaków: 9";
+                            }
+                        }
+
+                        if($flag) {
+                            if(password_verify($pass,$row['password'])) {
+                                $sqlUpdate = "UPDATE `users` SET `login` = '$login', `mail` = '$mail', `name` = '$name', `surname` = '$surname', `number` = '$number', `s_name` = '$s_name' WHERE id='$id_user'";;
+                                if(@$conn->query($sqlUpdate)) {
+                                    if(isset($_SESSION['data_error']))unset($_SESSION['data_error']);
+                                    if(isset($_SESSION['datae_update']))unset($_SESSION['datae_update']);
+
+                                    $result->close();
+                                    $conn->close();
+
+                                    $_SESSION['data_correct'] = 'Dane zostały zatkualizowane!';
+                                    header('Location: panel.php');
+                                    exit();
+                                }
+                                else {
+                                    $_SESSION['data_error'] = 'Error: Błąd zapytania do bazy!';
+                                }
+                            }
+                            else {
+                                $_SESSION['datae_update'] = 'Niepoprawne hasło';
+                            }
+                        }
+                        else {
+                            $_SESSION['datae_update'] = "Upewnij się, czy wpisane dane są poprawne";
+                        }
+                    }
+                    else {
+                        $_SESSION['data_error'] = 'Error: Błąd bazy danych!';
+                    }
+                }
+                else {
+                    $_SESSION['data_error'] = 'Error: Błąd bazy danych!';
+                }
+
+                $result->close();
+        }
+
+        else {
+            $_SESSION['data_error'] = 'Error: Błąd zapytania do bazy!';
+        }
+
+
+        $_SESSION['dataf_mail'] = $mail;
+        $_SESSION['dataf_login'] = $login;
+        $_SESSION['dataf_name'] = $name;
+        $_SESSION['dataf_number'] = $number;
+        $_SESSION['dataf_surname'] = $surname;
+        $_SESSION['dataf_s_name'] = $s_name;
+
+        $conn->close();
+    }
+
+    header('Location: panel.php');
+    exit();
+?>
--- panel.php
+++ panel.php
@@ -5,6 +5,43 @@
         header('Location: index.php');
         exit();
     }
+    require_once('connect.php');
+    
+    $conn = @new mysqli($host, $db_user, $db_password, $db_name);
+    if ($conn->connect_errno) {
+        $_SESSION['l_error'] = "Error: ".$conn->connect_errno;
+    }
+    else {
+        $id_user = $_SESSION['id_copy'];
+
+        $sql = "SELECT * FROM `users` WHERE id='$id_user'";
+        if($result = @$conn->query($sql)) {
+            $resultCheck = $result->num_rows;
+                if($resultCheck > 0) {
+                    $row = $result->fetch_assoc();
+
+                    $_SESSION['online'] = true;
+                    $_SESSION['type_copy'] = $row['type'];
+                    $_SESSION['login_copy'] = $row['login'];
+                    $_SESSION['mail_copy'] = $row['mail'];
+                    $_SESSION['name_copy'] = $row['name'];
+                    $_SESSION['surname_copy'] = $row['surname'];
+                    $_SESSION['number_copy'] = $row['number'];
+                    $_SESSION['s_name_copy'] = $row['s_name'];
+
+                }
+                else {
+                    $_SESSION['l_error'] = 'Error: Błąd bazy danych!';
+                }
+
+                $result->close();
+        }
+        else {
+            $_SESSION['l_error'] = 'Error: Błąd zapytania do bazy!';
+        }
+
+        $conn->close();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -33,6 +70,12 @@
 </head>
 <body>
     <div id="page-wrapper">
+        <div class="overlay-pass"></div>
+        <noscript>
+            <div id="no-scirpt-cnt">Gra Snake wymaga JavaScript do niezbędnego funkcjowania! <span>
+            Potrzebujesz pomocy, jak włączyć JavaScript? <a href="https://enable-javascript.com/" target="_blank">Przejdź tutaj.</a></span></div>
+            <div id="overlay"></div>
+        </noscript>
         <div id="page-cnt-wrapper">
             <main class="d-flex">
                 <div id="account-wrapper" class="container-fluid p-0">
@@ -45,8 +88,8 @@
                                         <img src="img/defult-account.jpg" alt="defult-photo">
                                     </div>
                                     <div id="account-info-user" class="col-9">
-                                        <div id="account-info-login"><?php echo $_SESSION['login_copy']?></div>
-                                        <div id="account-info-group">User</div>
+                                        <div id="account-info-login"><?php if($_SESSION['s_name_copy']=="login"){echo $_SESSION['login_copy'];}else {if(($_SESSION['name_copy']=='')&&($_SESSION['surname_copy']=='')){echo 'User '.$id_user;}else{echo $_SESSION['name_copy'].' '.$_SESSION['surname_copy'];}}?></div>
+                                        <div id="account-info-group"><?php echo $_SESSION['type_copy']?></div>
                                     </div>
                                 </div>
                                 <div class="system-account-options"><a href="panel.php"><i class="icon-home"></i><span>Strona główna</span></a></div>
@@ -55,63 +98,91 @@
                             </div>
                         </div>
                         <div id="system-account-container" class="col-10">
-                            <div id="system-account-info"><i class="icon-user"></i> Użytkownik: <?php echo $_SESSION['login_copy']?></div>
+                            <div id="system-account-info"><i class="icon-user"></i> Użytkownik: <?php if($_SESSION['s_name_copy']=="login"){echo $_SESSION['login_copy'];}else {if(($_SESSION['name_copy']=='')&&($_SESSION['surname_copy']=='')){echo 'User '.$id_user;}else{echo $_SESSION['name_copy'].' '.$_SESSION['surname_copy'];}}?></div>
                             <div id="system-account-home" class="system-account-items col-12">
                                 <div class="system-account-wrapper col-12">
                                     <div id="account-photo"><img src="img/defult-account.jpg" alt="defult-photo"/></div>
                                     <div id="account-info">
-                                        <h4 id="user-name"><?php echo $_SESSION['login_copy']?></h4>
+                                        <h4 id="user-name"><?php if($_SESSION['s_name_copy']=="login"){echo $_SESSION['login_copy'];}else {if(($_SESSION['name_copy']=='')&&($_SESSION['surname_copy']=='')){echo 'User '.$id_user;}else{echo $_SESSION['name_copy'].' '.$_SESSION['surname_copy'];}}?></h4>
                                         <h3 id="user-group"><?php echo $_SESSION['type_copy']?></h3>
                                     </div>
                                     <div id="user-data">
                                         <h4 id="user-data-header">Dane konta</h4>
                                         <div id="cnt-user-data">
-                                            <form  id="change-data" class="col-11">
+                                            <form  id="change-data" action="change-data.php" class="col-11" method="POST">
+                                                <div class="wrapper-user-data row feedback">
+                                                    <div class="name-user-data col-4"></div>
+                                                    <div class="box-user-data col-8">
+                                                        <div class="wrapp-input">
+                                                            <div class="invalid-feedback<?php if(isset($_SESSION['datae_update'])||isset($_SESSION['data_error'])){echo ' invalid-visible';}else if (isset($_SESSION['data_correct'])){echo ' invalid-visible correct';}?>"><?php if(isset($_SESSION['datae_update'])){echo $_SESSION['datae_update'];}else if(isset($_SESSION['data_error'])){echo $_SESSION['data_error'];}else if (isset($_SESSION['data_correct'])){echo $_SESSION['data_correct'];}?></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                                 <div class="wrapper-user-data row">
                                                     <div class="name-user-data col-4">Login</div>
                                                     <div class="box-user-data col-8">
-                                                        <input class="form-control" type="text" name="login" value="<?php echo $_SESSION['login_copy']?>" disabled/>
+                                                        <div class="wrapp-input<?php if(isset($_SESSION['edata_login'])) echo ' alert-validate'?>">
+                                                            <div class="error-message"><?php if(isset($_SESSION['edata_login'])) echo $_SESSION['edata_login']?></div>
+                                                            <input class="form-control <?php if(isset($_SESSION['edata_login'])) echo ' is-invalid'?>" type="text" name="data_login" <?php if(isset($_SESSION['dataf_login'])){echo "value='".$_SESSION['dataf_login']."'";} else if(isset($_SESSION['login_copy'])){echo "value='".$_SESSION['login_copy']."'";}?>/>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row">
                                                     <div class="name-user-data col-4">E-mail</div>
                                                     <div class="box-user-data col-8">
-                                                        <input class="form-control" type="text" name="mail" value="<?php echo $_SESSION['mail_copy']?>" disabled/>
+                                                        <div class="wrapp-input<?php if(isset($_SESSION['edata_mail'])) echo ' alert-validate'?>">
+                                                            <div class="error-message"><?php if(isset($_SESSION['edata_mail'])) echo $_SESSION['edata_mail']?></div>
+                                                            <input class="form-control <?php if(isset($_SESSION['edata_mail'])) echo ' is-invalid'?>" type="text" name="data_mail" <?php if(isset($_SESSION['dataf_mail'])){echo "value='".$_SESSION['dataf_mail']."'";} else if(isset($_SESSION['mail_copy'])){echo "value='".$_SESSION['mail_copy']."'";}?>/>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row">
-                                                    <div class="name-user-data col-4">Imie</div>
+                                                    <div class="name-user-data col-4">Imie<p>(opcjonalnie)</p></div>
                                                     <div class="box-user-data col-8">
-                                                        <input class="form-control" type="text" name="name" value="<?php ?>"/>
+                                                        <input class="form-control" type="text" name="data_name" <?php if(isset($_SESSION['dataf_name'])){echo "value='".$_SESSION['dataf_name']."'";} else if(isset($_SESSION['name_copy'])){echo "value='".$_SESSION['name_copy']."'";}?>/>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row">
-                                                    <div class="name-user-data col-4">Nazwisko</div>
+                                                    <div class="name-user-data col-4">Nazwisko<p>(opcjonalnie)</p></div>
                                                     <div class="box-user-data col-8">
-                                                        <input class="form-control" type="text" name="surname" value="<?php ?>"/>
+                                                        <input class="form-control" type="text" name="data_surname" <?php if(isset($_SESSION['dataf_surname'])){echo "value='".$_SESSION['dataf_surname']."'";} else if(isset($_SESSION['surname_copy'])){echo "value='".$_SESSION['surname_copy']."'";}?>/>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row">
-                                                    <div class="name-user-data col-4">Numer</div>
+                                                    <div class="name-user-data col-4">Numer<p>(opcjonalnie)</p></div>
                                                     <div class="box-user-data col-8">
-                                                        <input class="form-control" type="text" name="number" value="<?php ?>"/>
+                                                        <div class="wrapp-input<?php if(isset($_SESSION['edata_number'])) echo ' alert-validate'?>">
+                                                            <div class="error-message"><?php if(isset($_SESSION['edata_number'])) echo $_SESSION['edata_number']?></div>
+                                                            <input class="form-control <?php if(isset($_SESSION['edata_number'])) echo ' is-invalid'?>" type="tel" name="data_number" <?php if(isset($_SESSION['dataf_number'])){echo "value='".$_SESSION['dataf_number']."'";} else if(isset($_SESSION['number_copy'])){echo "value='".$_SESSION['number_copy']."'";}?>/>
+                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row">
                                                     <div class="name-user-data col-4">Nazwa użytkownika</div>
                                                     <div class="box-user-data col-8">
                                                         <div class="custom-control custom-radio">
-                                                            <input type="radio" class="custom-control-input" id="customControlValidation1" name="radio-stacked" required <?php if(isset($_SESSION['s_name_copy'])){if($_SESSION['s_name_copy']=="login") echo 'checked';}?>>
+                                                            <input type="radio" class="custom-control-input" id="customControlValidation1" name="radio-stacked" value="login" required <?php if(isset($_SESSION['dataf_s_name'])){if($_SESSION['dataf_s_name']=="login") echo 'checked';} else if(isset($_SESSION['s_name_copy'])){if($_SESSION['s_name_copy']=="login") echo 'checked';}?>>
                                                             <label class="custom-control-label" for="customControlValidation1">Login</label>
                                                         </div>
                                                         <div class="custom-control custom-radio">
-                                                            <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" required <?php if(isset($_SESSION['s_name_copy'])){if($_SESSION['s_name_copy']=="n&sn") echo 'checked';}?>>
+                                                            <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" value="n&sn" required <?php if(isset($_SESSION['dataf_s_name'])){if($_SESSION['dataf_s_name']=="n&sn") echo 'checked';} else if(isset($_SESSION['s_name_copy'])){if($_SESSION['s_name_copy']=="n&sn") echo 'checked';}?>>
                                                             <label class="custom-control-label" for="customControlValidation2">Imie i nazwisko</label>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="wrapper-user-data row justify-content-end">
-                                                    <button type="submit" class="btn btn-primary col-3">Zmień dane</button>
+                                                    <button type="button" class="btn btn-primary col-3 change-data-btn">Zmień dane</button>
+                                                </div>
+                                                <div id="passowrd-confirm" class="col-4">
+                                                    <div id="passowrd-confirm-close"><i class="icon-cancel"></i></div>
+                                                    <div id="paddowrd-confirm-cnt" class="col-11">
+                                                        <h5>POTWIERDŹ ZMIANE</h5>
+                                                        <div class="wrapp-input">
+                                                            <input class="form-control" type="password" name="data_pass" placeholder="Hasło"/>
+                                                            <span class="icon-input"><i class="icon-lock"></i></span>
+                                                        </div>
+                                                        <button type="submit" class="btn btn-primary change-data-btn-confirm">Zmień dane</button>
+                                                    </div>
                                                 </div>
                                             </form>
                                         </div>
@@ -203,7 +274,36 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="visual-effects.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.change-data-btn').on('click',function(e){
+                e.preventDefault();
+                $('#passowrd-confirm').addClass('passowrd-confirm-show');
+                $('.overlay-pass').addClass('overlay-pass-show');
+            });
+
+            $('.overlay-pass, #passowrd-confirm-close').on('click', function(){
+                $('#passowrd-confirm').removeClass('passowrd-confirm-show');
+                $('.overlay-pass').removeClass('overlay-pass-show');
+            });
+        });
+    </script>
     <?php 
+        if(isset($_SESSION['data_correct']))unset($_SESSION['data_correct']);
+        if(isset($_SESSION['data_error']))unset($_SESSION['data_error']);
+        if(isset($_SESSION['datae_update']))unset($_SESSION['datae_update']);
+        if(isset($_SESSION['edata_mail']))unset($_SESSION['edata_mail']);
+        if(isset($_SESSION['dataf_mail']))unset($_SESSION['dataf_mail']);
+        if(isset($_SESSION['edata_login']))unset($_SESSION['edata_login']);
+        if(isset($_SESSION['dataf_login']))unset($_SESSION['dataf_login']);
+        if(isset($_SESSION['edata_number']))unset($_SESSION['edata_number']);
+        if(isset($_SESSION['dataf_number']))unset($_SESSION['dataf_number']);
+
+        if(isset($_SESSION['dataf_name']))unset($_SESSION['dataf_name']);
+        if(isset($_SESSION['dataf_surname']))unset($_SESSION['dataf_surname']);
+        if(isset($_SESSION['dataf_s_name']))unset($_SESSION['dataf_s_name']);
+
+
         if(isset($_SESSION['d_error']))unset($_SESSION['d_error']);
         if(isset($_SESSION['de_logon']))unset($_SESSION['de_logon']);
 
